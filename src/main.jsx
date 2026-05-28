399b45cc1b00690192567dbb4e019773aa9c689a
digweze-bit <digweze@gmail.com>
2026-05-28 19:38:48 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HourglassGallery from './HourglassGallery'
-ReactDOM.createRoot(document.getElementById('root')).render(<HourglassGallery />)
+import HourglassAdmin from './HourglassAdmin'
+
+const isAdmin = window.location.pathname === '/admin'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  isAdmin ? <HourglassAdmin /> : <HourglassGallery />
+)
